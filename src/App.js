d448a89ba06082e0cddb39e83bbac1f1e69346f1
Handhika Dwi <handhika@oloopstudio.com>
2021-12-06 08:12:32 +0700

    init react crossword, react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Crossword from '@jaredreisinger/react-crossword';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const data = {
+        across: {
+            1: {
+              clue: 'one plus one',
+              answer: 'TWO',
+              row: 0,
+              col: 0,
+            },
+          },
+          down: {
+            2: {
+              clue: 'three minus two',
+              answer: 'ONE',
+              row: 0,
+              col: 2,
+            },
+          }
+    };
+
+    return (
+        <>
+            <Crossword data={data} />
+        </>
+    )
 }
 
 export default App;
