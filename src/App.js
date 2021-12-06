87f23ce0fc7c871631d32ec6b5184f97794d03e0
Handhika Dwi <handhika@oloopstudio.com>
2021-12-06 08:50:55 +0700

    setup crossword

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
 import Crossword from '@jaredreisinger/react-crossword';
+import { useState, useRef } from 'react'
+import { Modal } from 'react-bootstrap'
 
 const App = () => {
+
+    const [modalWrongAnswer, setModalWrongAnswer] = useState(false)
+
+    const crossWordRef = useRef()
+
+    const reset = () => {
+        if (crossWordRef.current) {
+            crossWordRef.current.reset()
+        }
+    }
+
+    const fillAllAnswer = () => {
+        if (crossWordRef.current) {
+            crossWordRef.current.fillAllAnswers()
+        }
+    }
+
     const data = {
         across: {
-            1: {
-              clue: 'one plus one',
-              answer: 'TWO',
-              row: 0,
-              col: 0,
-            },
+            1: { clue: 'Buah yang dipotong-potong, diuleg campur gula jawa dan cabai, jadinya? Lorem ipsum dolor sit amet', answer: 'rusa', row: 0, col: 0 },
+            4: { clue: 'is', answer: 'XXX', row: 0, col: 4 },
+            7: { clue: 'not', answer: 'XXX', row: 1, col: 0 },
+            8: { clue: 'a', answer: 'XXXX', row: 1, col: 4 },
+            10: { clue: 'real', answer: 'XX', row: 2, col: 0 },
+            11: { clue: 'crossword,', answer: 'XX', row: 2, col: 3 },
+            12: { clue: 'it', answer: 'XX', row: 2, col: 6 },
+            13: { clue: 'is', answer: 'XXXXXX', row: 3, col: 0 },
+            16: { clue: 'only', answer: 'XXXXXX', row: 4, col: 2 },
+            19: { clue: 'showing', answer: 'XX', row: 5, col: 0 },
+            21: { clue: 'the', answer: 'XX', row: 5, col: 3 },
+            22: { clue: 'kind', answer: 'XX', row: 5, col: 6 },
+            23: { clue: 'of', answer: 'XXXX', row: 6, col: 0 },
+            25: { clue: 'thing', answer: 'XXX', row: 6, col: 5 },
+            26: { clue: 'you', answer: 'XXX', row: 7, col: 1 },
+            27: { clue: 'can', answer: 'XXX', row: 7, col: 5 },
           },
           down: {
-            2: {
-              clue: 'three minus two',
-              answer: 'ONE',
-              row: 0,
-              col: 2,
-            },
-          }
+            1: { clue: 'Buah yang dipotong-potong, diuleg campur gula jawa dan cabai, jadinya? Lorem ipsum dolor sit amet.', answer: 'XXXX', row: 0, col: 0 },
+            2: { clue: 'All', answer: 'XXXX', row: 0, col: 1 },
+            3: { clue: 'of', answer: 'XX', row: 0, col: 2 },
+            4: { clue: 'the', answer: 'XXXXXX', row: 0, col: 4 },
+            5: { clue: 'answers', answer: 'XX', row: 0, col: 5 },
+            6: { clue: 'are', answer: 'XXX', row: 0, col: 6 },
+            9: { clue: '"X"', answer: 'XX', row: 1, col: 7 },
+            11: { clue: "test", answer: 'XXXXXX', row: 2, col: 3 },
+            14: { clue: "test", answer: 'XX', row: 3, col: 2 },
+            15: { clue: "test", answer: 'XX', row: 3, col: 5 },
+            17: { clue: "test", answer: 'XXXX', row: 4, col: 6 },
+            18: { clue: "test", answer: 'XXXX', row: 4, col: 7 },
+            19: { clue: "test", answer: 'XX', row: 5, col: 0 },
+            20: { clue: "test", answer: 'XXX', row: 5, col: 1 },
+            24: { clue: "test", answer: 'XX', row: 6, col: 2 },
+            25: { clue: "test", answer: 'XX', row: 6, col: 5 },
+        },
     };
 
     return (
         <>
-            <Crossword data={data} />
+            <Crossword 
+                ref={crossWordRef}
+                data={data}
+            />
+
+            <div className="pe-5" style={{
+                opacity: 1,
+                marginTop: -100
+            }}>
+                <div className="d-flex justify-content-end">
+                    <button className="btn btn-danger me-5 px-4" onClick={() => setModalWrongAnswer(true)}>Salah</button>
+                    <button className="btn btn-secondary me-5 px-4" onClick={reset}>Reset</button>
+                    <button className="btn btn-success me-5 px-4" onClick={fillAllAnswer}>Fill</button>
+                </div>
+            </div>
+
+            <Modal size="lg" centered show={modalWrongAnswer} onHide={() => setModalWrongAnswer(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Salah!!!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <button className="btn btn-secondary" onClick={() => setModalWrongAnswer(false)}>Tutup</button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
